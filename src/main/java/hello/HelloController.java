6a07d768b05bfcbfa06e93acb6cf71a55693a1af
Jagadeesh-now <157399582+Jagadeesh-now@users.noreply.github.com>
2024-11-08 13:02:10 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
         }
 
         // Return the greeting message with the IPv4 address
-        return "Greetings from  'vignan' deployed JAVA app in virtual Machine..!!! Host private IPv4 Address: " + ipAddress;
+        return "Greetings from  'JAGADEESH BABU' deployed JAVA app in ELASTICBEANS STALK..!!! Host private IPv4 Address: " + ipAddress;
     }
     
 }
